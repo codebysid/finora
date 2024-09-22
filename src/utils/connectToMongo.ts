066f1cdf3e1272b8a05ffd68cf64a3356da6181d
codebysid <codebysidd@gmail.com>
2024-09-22 21:41:28 +0530

    fixed mongo state

--- src/utils/connectToMongo.ts
+++ src/utils/connectToMongo.ts
@@ -1,5 +1,8 @@
-import { connect } from "mongoose";
+import { connect, connection } from "mongoose";
 
 export default async function connectToMongo() {
+  if (connection.readyState >= 1) {
+    return;
+  }
   await connect(process.env.MONGODB_URI as string);
 }
